89898ef6ae4b63d3be8aa2a018ab2198852016f7
Pogbayoe <adebayooluwasegun335@gmail.com>
2024-12-19 15:37:56 +0100

    updated ui

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,29 @@
+import { useContext } from 'react';
+import Card from './shared/Card';
+import FeedbackContext from '../context/FeedbackContext';
+import { FaTimes, FaEdit } from 'react-icons/fa'; // Import the necessary icon components
+
+function FeedbackItem({ item }) {
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext);
+
+  if (!item || !item.rating) {
+    return <div>Loading or no data available</div>;
+  }
+
+  return (
+    <Card>
+      <div className="num-display">{item.rating}</div>
+
+      <button onClick={() => deleteFeedback(item.id)} className='close'>
+        <FaTimes color='purple' /> 
+      </button>
+      <button onClick={() => editFeedback(item)} className='edit'>
+        <FaEdit color='purple' /> 
+      </button>
+
+      <div className="text-display">{item.text} </div>
+    </Card>
+  );
+}
+
+export default FeedbackItem;
